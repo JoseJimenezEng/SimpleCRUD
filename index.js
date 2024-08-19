35cb6b78600288a0d81f3b47ec593f11ca9b1ae4
JoseJimenezEng <jjmarts@gmail.com>
2024-08-19 00:29:52 -0500

    Updated cors URL to local test

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const personaRoutes = require('./routes/personaRoutes');
 const cors = require('cors');  
 
 const app = express();
-app.use(cors()); 
 
+app.use(cors({
+  origin: 'http://localhost:5173'
+}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
