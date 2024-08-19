a3b41be1ee24aa1c858ac9883d8bc7acf9a53e8b
JoseJimenezEng <jjmarts@gmail.com>
2024-08-19 01:23:12 -0500

    added URL to server

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'https://clientcrud.netlify.app'
 }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
